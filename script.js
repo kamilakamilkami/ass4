7307b063fd0a05d2e45cb3e631e19c5ccbae8331
kamilakamilkami <nurlybayevakamila@gmail.com>
2024-10-18 02:30:09 +0600

    1st commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeBg(){
+    document.body.style.backgroundColor = "pink" ;
+}
+
+function changeImg(){
+    document.getElementById("image").src = "nigga.jpg"
+}
+
+function displayMsg(){
+    document.getElementById("message").innerHTML = "Hello this is me!"
+}
+
+
